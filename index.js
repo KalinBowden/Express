8ae59f26f4eb499a9b6999331142d8d673b59a17
kalinbowden <kalinbowden@gmail.com>
2017-11-08 09:05:54 -0600

    Hello github

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+    Kalin Bowden
+*/
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+ 
+app.get('/', function (req, res) 
+{
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+
+app.get('/about', function (req, res) 
+{
+  res.sendFile(path.join(__dirname + '/about.html'));
+})
+ 
+app.listen(3000);
